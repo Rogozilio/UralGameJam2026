59a614b263fc409bdf13765d7306678801d48931
Rogozilio <r.rogozin@newfolder.studio>
2026-06-14 23:48:40 +0700

    Fix killbox

--- Assets/Scripts/KillBox.cs
+++ Assets/Scripts/KillBox.cs
@@ -1,16 +1,39 @@
-using System;
+using Unity.Entities;
 using UnityEngine;
+using UralGameJam.Ecs.Physics3D;
 
 namespace Scripts
 {
+    public struct KillBoxTag : IComponentData
+    {
+    }
     public class KillBox : MonoBehaviour
     {
-        private void OnTriggerStay(Collider other)
+        private Entity _killBoxEntity;
+        private EntityManager _entityManager;
+
+        private void Awake()
         {
-            if(other.CompareTag("Player"))
-            {
-                other.GetComponent<Player>().RestartNow();
-            }
+            _entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+            _killBoxEntity = _entityManager.CreateEntity(typeof(KillBoxTag));
+        
+            EnsureTriggerBridge();
+        }
+        
+        private void OnDestroy()
+        {
+            if (_entityManager.Exists(_killBoxEntity))
+                _entityManager.DestroyEntity(_killBoxEntity);
+        }
+
+        private void EnsureTriggerBridge()
+        {
+            var provider = GetComponent<ColliderAndTriggerDOTSProvider>();
+
+            if (provider == null)
+                provider = gameObject.AddComponent<ColliderAndTriggerDOTSProvider>();
+
+            provider.entityA = _killBoxEntity;
         }
     }
 }
