10e16e620b15950261321123103eed3133306d92
Rogozilio <r.rogozin@newfolder.studio>
2026-06-28 00:51:11 +0700

    fix lifeTime

--- Assets/Scripts/LifeTime.cs
+++ Assets/Scripts/LifeTime.cs
@@ -27,6 +27,8 @@ namespace Scripts
                 IsFastTime = false
             });
             _entityManager.SetComponentEnabled<LifeTimeComponent>(_entity, true);
+            _entityManager.AddComponent<LifeTimePausedTag>(_entity);
+            _entityManager.SetComponentEnabled<LifeTimePausedTag>(_entity, false);
 
             if (!_entityManager.HasComponent<RestartFireRequestTag>(_entity))
                 _entityManager.AddComponent<RestartFireRequestTag>(_entity);
@@ -41,6 +43,9 @@ namespace Scripts
 
                 if (_entityManager.HasComponent<LifeTimeComponent>(_entity))
                     _entityManager.RemoveComponent<LifeTimeComponent>(_entity);
+
+                if (_entityManager.HasComponent<LifeTimePausedTag>(_entity))
+                    _entityManager.RemoveComponent<LifeTimePausedTag>(_entity);
             }
         }
     }
